d44b438ad5a8f1f91fe3c7b3fe1088ada8a45765
JustKevinn <kevinrodriguez7411@gmail.com>
2022-06-19 14:27:08 -0300

    First functions on ruby

--- ruby-l.rb
+++ ruby-l.rb
@@ -1,1 +1,13 @@
-print "Learning RUBY\n"
+#puts "im Learning RUBY" (first line)
+
+# --- functions ---
+
+def generar_saludo(name)
+    "Hi,im #{name}"  #Popular 2 spaces in the language ruby
+end
+def generar_continuacion(cont)
+    "and im learning #{cont}"
+end
+
+puts generar_saludo("Kevin")
+puts generar_continuacion("RUBY")
